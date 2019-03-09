bb9aebdb738d95bc9cb9927f65fd37408c2c8b76
Angelika Meraner <angelika.meraner@statistik.gv.at>
2019-03-09 11:47:08 +0000

    bigo

--- bigo_import.R
+++ bigo_import.R
@@ -17,6 +17,13 @@ tables_b <- lapply(
 tables_c <- lapply(
   list.files("/dataset/bigo/tables/table_c/", full.names = TRUE), 
   function(x) {
-    readr::read_csv(x, ) %>% mutate(file = x)
+    readr::read_csv(x) %>% mutate(file = x)
   }
 ) %>% data.table::rbindlist()
+
+
+# character id
+#tables_a[ ,id:=paste0("pid",substr(file,start=31, stop=33))]
+# numeric id
+tables_a[ ,id:=as.numeric(substr(file,start=31, stop=33))]
+tables_a[,time_]
