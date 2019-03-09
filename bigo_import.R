180e9e0822c80d8f0052e3aaf6f2425f904af1ef
Angelika Meraner <angelika.meraner@statistik.gv.at>
2019-03-09 19:05:38 +0000

    pfade essen

--- bigo_import.R
+++ bigo_import.R
@@ -1,21 +1,21 @@
 library(dplyr)
 
 tables_a <- lapply(
-  list.files("/dataset/bigo/tables/table_a/", full.names = TRUE), 
+  list.files("/mnt/s3/bigO/tables/table_a/", full.names = TRUE), 
   function(x) {
     readr::read_csv(x) %>% mutate(file = x)
   }
 ) %>% data.table::rbindlist()
 
 tables_b <- lapply(
-  list.files("/dataset/bigo/tables/table_b/", full.names = TRUE), 
+  list.files("/mnt/s3/bigO/tables/table_b/", full.names = TRUE), 
   function(x) {
     readr::read_csv(x) %>% mutate(file = x)
   }
 ) %>% data.table::rbindlist()
 
 tables_c <- lapply(
-  list.files("/dataset/bigo/tables/table_c/", full.names = TRUE), 
+  list.files("/mnt/s3/bigO/tables/table_c/", full.names = TRUE), 
   function(x) {
     readr::read_csv(x) %>% mutate(file = x)
   }
@@ -27,3 +27,25 @@ tables_c <- lapply(
 tables_a[ ,id:=as.numeric(substr(file,start=31, stop=33))]
 tables_b[ ,id:=as.numeric(substr(file,start=31, stop=33))]
 tables_c[ ,id:=as.numeric(substr(file,start=31, stop=33))]
+
+meals <- lapply(
+  list.files("/mnt/s3/bigO/self_reports"),
+  
+  function(x) {
+    meal <- strsplit(grep("jpg",list.files(paste0("/mnt/s3/bigO/self_reports/",x,"/meals")),value=TRUE),".",fixed=TRUE)
+    if(length(meal)>0){
+    meal <- unlist(sapply(meal,function(x)x[[1]]))
+    meal_paths <- paste0(paste0("/mnt/s3/bigO/self_reports/",x,"/meals/",meal,".jpg"))
+    meals <- cbind(meal,meal_paths,pid=x)
+    }else{
+      meals <- NULL
+  }
+}
+)
+meals <- do.call(rbind, meals)
+
+data.table::setDT(meals)
+
+
+list.files("/mnt/s3/bigO/self_reports/819/meals")
+help(package="jsonlite")
