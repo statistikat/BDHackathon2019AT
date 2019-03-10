2e69c74a09541d8f8317a99a31c9735b11457c7c
Gregor de Cillia <gregor.decillia@statistik.gv.at>
2019-03-10 15:28:51 +0000

    use combodat for trelliscope

--- bigo/trelliscope.R
+++ bigo/trelliscope.R
@@ -1,22 +1,25 @@
-source(usethis::proj_path("bigo/bigo_import.R"))
+source(usethis::proj_path("bigo/combidat.R"))
 library(trelliscopejs)
 
 meal_img_url <- function(user_id, meal_id) {
   paste0("http://52.214.106.253/img/meals/", user_id, "_", meal_id, ".jpg")
 }
 
-meals2 <- merge(
-  tables_c[, list(activity = sum(foot+bike, na.rm = TRUE)), by = pid],
-  meals_json
-) 
-
-meals2 <- meals2 %>% 
-  mutate(id = 1:nrow(meals2), img = meal_img_url(pid, meal), 
+meals2 <- combidat %>%
+  filter(!is.na(meal)) %>% 
+  mutate(img = meal_img_url(pid, meal), 
          day = lubridate::wday(start_datetime, label = TRUE),
-         hour = lubridate::hour(start_datetime)) %>% select(
-           meal_type, day, img, hour, temperature,
-           home_prepared = meal_attributes.home_prepared, sugar
-         )
+         hour = lubridate::hour(start_datetime), 
+         steps_cat = as.factor(steps_cat), 
+         country = iso2,
+         fitness_cat = as.factor(fitness_cat)
+         ) %>% 
+  select(
+    meal_type, day, img, hour, temperature,
+    home_prepared = meal_attributes.home_prepared, sugar,
+    median_fitness, median_steps, fitness_cat, steps_cat, country#,
+    #id
+  )
 
 ts <- meals2 %>% mutate(img = img_panel(img)) %>%
   trelliscope(
